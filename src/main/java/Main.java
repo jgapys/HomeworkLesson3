17fb265129d8375e340c31d2c9a354ce07b82e17
jgapys <jgapys@sii.pl>
2022-08-16 02:41:51 +0200

    Adding validation of user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,14 +19,26 @@ public class Main {
 
         System.out.println("Enter employee's first name");
         String firstName = scanner.nextLine();
+        while (!UserInputHandler.checkFirstNameAndLastNameCorrectness(firstName)) {
+            System.out.println("Enter the correct first name");
+            firstName = scanner.nextLine();
+        }
 
         System.out.println("Enter employee's last name");
         String lastName = scanner.nextLine();
+        while (!UserInputHandler.checkFirstNameAndLastNameCorrectness(lastName)) {
+            System.out.println("Enter the correct last name");
+            lastName = scanner.nextLine();
+        }
 
         System.out.println("Enter employee's salary");
-        float salary = scanner.nextFloat();
+        String salary = scanner.nextLine();
+        while (!UserInputHandler.checkSalaryCorrectness(salary)) {
+            System.out.println("Enter the correct salary value");
+            salary = scanner.nextLine();
+        }
 
-        return new Employee(firstName, lastName, salary);
+        return new Employee(firstName, lastName, Float.parseFloat(salary));
     }
 
     private static void addNewEmployee(int newEmployeesNumber, Company company) {
@@ -34,28 +46,23 @@ public class Main {
             System.out.println("Adding new employee");
             company.allEmployees.add(getEmployeeFromUser());
             System.out.println("Added new employee");
+            System.out.println();
         }
     }
 
     private static void chooseMenuOption(Company company) {
-        Scanner scanner = new Scanner(System.in);
         boolean nextOption = true;
 
         while (nextOption) {
-            System.out.println("What do you want to do next? Please choose one of the following by entering the correct number:");
+            System.out.println("What do you want to do? Please choose one of the following by entering the correct number:");
             System.out.println("1 - Print sum of all employees salary");
             System.out.println("2 - Display all employees data");
             System.out.println("3 - Add new employee");
             System.out.println("4 - End program");
 
-            int userChoice = scanner.nextInt();
-            if (!UserInputHandler.checkMenuOptions(userChoice)) {
-                userChoice = scanner.nextInt();
-            }
-
-            switch (userChoice) {
-                case 1 -> System.out.println("The sum of salaries of all employees is: "
-                        + company.allEmployees.stream().mapToDouble(Employee::getSalary).sum());
+            switch (UserInputHandler.checkMenuChoice()) {
+                case 1 ->
+                        System.out.println("The sum of salaries of all employees is: " + company.allEmployees.stream().mapToDouble(Employee::getSalary).sum());
                 case 2 -> company.allEmployees.forEach(System.out::println);
                 case 3 -> addNewEmployee(1, company);
                 case 4 -> {
---
+++ src/main/java/UserInputHandler.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInputHandler {
+    public static boolean checkFirstNameAndLastNameCorrectness(String userInput) {
+        String regEx = "^[a-zA-Z ]*$";
+        return userInput.matches(regEx);
+    }
+
+    public static boolean checkSalaryCorrectness(String userInput) {
+        String regEx = "\\d+(\\.\\d+)?";
+        return userInput.matches(regEx);
+    }
+
+    public static int checkMenuChoice() {
+        int menu = 0;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int[] possibleMenuOptions = {1, 2, 3, 4};
+            int userMenuChoice = scanner.nextInt();
+            if (Arrays.stream(possibleMenuOptions).anyMatch(i -> i == userMenuChoice)) {
+                menu = userMenuChoice;
+            } else {
+                System.out.println("Sorry, you entered the wrong number. The number must be 1, 2, 3 or 4.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Sorry, you entered the wrong type of data. The entered data should be the number 1, 2, 3 or 4.");
+        }
+        return menu;
+    }
+}
