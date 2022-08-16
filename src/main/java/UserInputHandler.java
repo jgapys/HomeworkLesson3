fec913285591d8c7e2b7616deb6d598a4a03e6f6
jgapys <jgapys@sii.pl>
2022-08-16 08:27:55 +0200

    Changing the name of a variable

--- src/main/java/UserInputHandler.java
+++ src/main/java/UserInputHandler.java
@@ -14,19 +14,19 @@ public class UserInputHandler {
     }
 
     public static int checkMenuChoice() {
-        int menu = 0;
+        int menuOption = 0;
         Scanner scanner = new Scanner(System.in);
         try {
             int[] possibleMenuOptions = {1, 2, 3, 4};
             int userMenuChoice = scanner.nextInt();
             if (Arrays.stream(possibleMenuOptions).anyMatch(i -> i == userMenuChoice)) {
-                menu = userMenuChoice;
+                menuOption = userMenuChoice;
             } else {
                 System.out.println("Sorry, you entered the wrong number. The number must be 1, 2, 3 or 4.");
             }
         } catch (InputMismatchException e) {
             System.out.println("Sorry, you entered the wrong type of data. The entered data should be the number 1, 2, 3 or 4.");
         }
-        return menu;
+        return menuOption;
     }
 }
